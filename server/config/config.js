471ddf15ce9a296592a44ee7802b890855dc30d7
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2019-10-13 17:31:18 -0500

    Modification to the port for Mongo Atlas and port LocalHost

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,20 @@
 PUERTO
 */
 process.env.PORT = process.env.PORT || 3000;
+
+/*
+ENTORNO
+*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/*
+Base de datos
+*/
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Victoriasaurio:U3mFQ0ddiNoShOAR@cluster0-2yvd4.mongodb.net/cafe';
+}
+process.env.URLDB = urlDB;
